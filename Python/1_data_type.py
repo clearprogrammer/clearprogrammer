871d57323de394897579b561b0aa2ee572ad43cf
clearprogrammer <89745870+clearprogrammer@users.noreply.github.com>
2021-08-31 11:49:08 +0530

    Prints data type

---
+++ Python/1_data_type.py
@@ -0,0 +1,15 @@
+# Print data type
+
+a = 1
+b = 'c'
+c = str
+d = '1c'
+e = 'stringex'
+#f = 1j # works in w3schools as complex type
+
+print (type(a))
+print (type(b))
+print (type(c))
+print (type(d))
+print (type(e))
+#print (type(f)) # works in w3schools as complex type
